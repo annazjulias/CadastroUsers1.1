2800263c15c2d09fc5e615b7f5fd1bd453f6747c
Annazjulias <anazjuliasilva15@gmail.com>
2025-08-14 08:08:10 -0300

    api.vercel

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://cadastro-users1-1.vercel.app/', // Porta do seu backend
+  baseURL: 'https://cadastro-dev-club2-c5uy.vercel.app', // Porta do seu backend
 });
 
 export default api;
